3960bcac018d3c7fed858ee6f81718e5e4329da0
Kirill <kirisa49@gmail.com>
2025-07-26 23:28:04 +0700

    server's (add, get_all)

--- app/api/server.py
+++ app/api/server.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException, Depends
+from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
 from uuid import UUID
 
@@ -6,10 +7,19 @@ from app.core.database import get_db
 from app.schemas.server import ServerAdd, ServerReponse
 from app.crud.server import ServerCRUD
 
-router = APIRouter()
+router = APIRouter(prefix="/servers", tags=["Сервера"])
 server_crud = ServerCRUD()
 
-@router.get("/servers/{server_uuid}", response_model=ServerReponse)
+
+@router.get("/", response_model=List[ServerReponse])
+async def get_servers(db: AsyncSession = Depends(get_db)):
+    servers = await server_crud.get_all_servers(db)
+
+    if not servers:
+        raise HTTPException(status_code=404, detail="Servers not found")
+    return servers
+
+@router.get("/{server_uuid}", response_model=ServerReponse)
 async def get_server(server_uuid: UUID, db: AsyncSession = Depends(get_db)):
     server_data = {"uuid": server_uuid}
     server = await server_crud.check_existing(db, server_data)
@@ -17,3 +27,14 @@ async def get_server(server_uuid: UUID, db: AsyncSession = Depends(get_db)):
     if not server:
         raise HTTPException(status_code=404, detail="Server not found")
     return server
+
+@router.post("/add", response_model=ServerReponse)
+async def add(server_data: ServerAdd, db: AsyncSession = Depends(get_db)):
+    is_existing = await server_crud.check_existing(db, server_data)
+
+    if is_existing:
+        raise HTTPException(status_code=400, detail="Server already exists")
+
+    server = await server_crud.add_server(db, server_data)
+    return server
+    
--- app/crud/server.py
+++ app/crud/server.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
-from typing import Optional
+from typing import Optional, List
 
 from app.models.server import Server
 from app.schemas.server import ServerAdd, ServerReponse
@@ -8,7 +8,7 @@ from app.schemas.server import ServerAdd, ServerReponse
 
 class ServerCRUD:
     @staticmethod
-    async def check_existing(db: AsyncSession, server_data: dict) -> Optional[ServerReponse]:
+    async def check_existing(db: AsyncSession, server_data: Server) -> Optional[ServerReponse]:
         query = select(Server).where(
             Server.uuid == server_data.get("uuid")
         )
@@ -19,3 +19,24 @@ class ServerCRUD:
             return None
         
         return ServerReponse.model_validate(existing_server)
+
+    @staticmethod
+    async def get_all_servers(db: AsyncSession) -> Optional[List[ServerReponse]]:
+        query = select(Server)
+        result = await db.execute(query)
+        servers = result.scalars().all()
+
+        if not servers:
+            return None
+        
+        return [ServerReponse.model_validate(server) for server in servers]
+        
+    @staticmethod
+    async def add_server(db: AsyncSession, server_data: ServerAdd) -> ServerReponse:
+        new_server = Server(**server_data.model_dump())
+        db.add(new_server)
+
+        await db.commit()
+        await db.refresh(new_server)
+
+        return ServerReponse.model_validate(new_server)
